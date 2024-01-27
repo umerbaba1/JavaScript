bfd0a8fcf98204d093f78c4d9d365c1c9380cb9d
umerbaba1 <umerbab567@gmail.com>
2024-01-27 12:18:03 +0530

    strings

--- strings.js
+++ strings.js
@@ -1,16 +1,35 @@
 let name ="Kounsain"
 console.log(name,name.length)
+
 //  strlength
 let th=name.length
 console.log(th)
+
 //str indices
 console.log(name[2],name[4],name[0])
+
 //str  method
 let newname=name.toUpperCase()
 console.log(newname)
 var lowname=name.toLowerCase()
 console.log(lowname)
+
 //   trim()
 var space="                      Apna college loop hole new in one                  "
 var space1=space.trim()
 console.log(space1,space)
+
+// slice
+let slice=name.slice(0,7)
+console.log(slice)
+
+// join strs
+let joined=space.concat(name)
+console.log(joined)
+
+// replace  
+let a="new name";
+let result =a.replace("new","old")
+console.log(result)
+
+// 
