bc6fa6f7431a91b24358bb5b587c0ec6268c22c3
umerbaba1 <umerbab567@gmail.com>
2024-01-27 11:37:07 +0530

    Strings added

---
+++ strings.js
@@ -0,0 +1,16 @@
+let name ="Kounsain"
+console.log(name,name.length)
+//  strlength
+let th=name.length
+console.log(th)
+//str indices
+console.log(name[2],name[4],name[0])
+//str  method
+let newname=name.toUpperCase()
+console.log(newname)
+var lowname=name.toLowerCase()
+console.log(lowname)
+//   trim()
+var space="                      Apna college loop hole new in one                  "
+var space1=space.trim()
+console.log(space1,space)
